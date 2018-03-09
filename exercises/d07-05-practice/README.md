e7b7a58066668ec1c279e494904f4256c8131fc4
JE <28377077+vnsn@users.noreply.github.com>
2018-03-09 09:09:52 -0700

    function practice

---
+++ exercises/d07-05-practice/README.md
@@ -0,0 +1,65 @@
+# Practice Problems
+
+1) Make a function that takes a string and returns that string reversed.
+
+Example
+
+Input: "Hello World" 
+Output: "dlroW olleH"
+
+let reverseStr = function(str) {
+
+}
+
+2) Make a function that takes a string and returns true if the string could be a number else return false.
+
+Example
+
+Input: "3" 
+Output: true
+
+Input: "three" 
+Output: false
+
+let isNum = function(str) {
+
+}
+
+3) Make a function that takes a number and returns true if the number is even else return false.
+
+Example
+
+Input: 4 
+Output: true
+
+Input: 3 
+Output: false
+
+let isEven = function(num) {
+
+}
+
+4) Make a function that takes an array and returns the average of the array.
+
+Example
+
+Input: [1, 2, 3] 
+Output: 2
+
+Input: [5, -10, 10, 20] 
+Output: 6.25
+
+let averageArray = function(arr) {
+
+}
+
+5) Make a function that takes two arrays and returns a single array of the items from the arrays added alternatingly.
+
+Example
+
+Input: ["a", "b", "c"] and [1, 2, 3] 
+Output: ["a", 1, "b", 2, "c", 3]
+
+let combineArrays = function(arr1, arr2) {
+
+}
---
+++ exercises/d07-05-practice/main.js
@@ -0,0 +1,87 @@
+// 1) Make a function that takes a string and returns that string reversed.
+
+function reverseStr(str) {
+    var myStr = "";
+
+    for (var i = str.length - 1; i >= 0; i--) {
+        myStr += str[i];
+    }
+    return myStr;
+}
+
+var testStr = "Hello World"
+console.log("\ntesting reserveStr");
+console.log(reverseStr(testStr));
+
+
+// 2) Make a function that takes a string 
+// and returns true if the string could be a number, else return false.
+
+function isNum(str) {
+ var test = str / 10;
+ console.log(test);
+ console.log(typeof(test));
+ if (Number.isNaN(test)) {
+     return false;
+ } else return true;
+}
+
+testNum1 = "3";
+testNum2 = "three";
+console.log("\ntesting isNum");
+console.log(isNum(testNum1));
+console.log(isNum(testNum2));
+
+
+
+// 3) Make a function that takes a number and returns true 
+// if the number is even, else return false.
+
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else return false;
+}
+
+console.log("\ntesting isEven");
+console.log(isEven(3));
+console.log(isEven(4));
+
+
+function arrAvg(arr) {
+    var sum = 0;
+    // add up all the elements.
+    // divide sum by length
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+testArr = [5, -10, 10, 20];
+
+console.log("\ntesting arrAvg");
+console.log(arrAvg(testArr));
+
+
+// 5) Make a function that takes two arrays and returns a single array 
+// of the items from the arrays added alternatingly.
+
+function combineArr(arr1, arr2) {
+    myArr = [];
+    var totalItems = arr1.length + arr2.length;
+
+    for (var i = 0; i < totalItems; i++) {
+        if (i < arr1.length) {
+            myArr.push(arr1[i]);
+        }
+        if (i < arr2.length) {
+            myArr.push(arr2[i]);
+        }
+        }
+    return myArr;
+    }
+
+testArr2 = ["a", "b", "c"];
+console.log("\ntesting combineArr");
+console.log(combineArr(testArr, testArr2));
