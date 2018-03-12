f466ffc4a9bdf366e52bedf019ea75fbb2b23835
JE <28377077+vnsn@users.noreply.github.com>
2018-03-12 10:05:39 -0600

    updated anti-caps

--- exercises/d09-04-anti-caps/main.js
+++ exercises/d09-04-anti-caps/main.js
@@ -1,15 +1,11 @@
-function isCaps(letter) {
-    return letter === letter.toUpperCase();
-}
 
 function switchCaps(str) {
     var resultStr = "";
 
     for (var i = 0; i < str.length; i++) {
-        if (isCaps(str[i])) {
+        if (str[i] === str[i].toUpperCase()) {
             resultStr += str[i].toLowerCase();
         } else resultStr += str[i].toUpperCase();
-
     }
     return resultStr;
 }
