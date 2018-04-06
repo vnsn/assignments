623c692d08dff3332f0c5a1ebf661eb40e95aea1
JE <28377077+vnsn@users.noreply.github.com>
2018-04-06 10:01:54 -0600

    turned getTheInfo into arrow fx so I don't have to bind it.

--- exercises/d26-05-react-wars/src/pages/DataDetails.js
+++ exercises/d26-05-react-wars/src/pages/DataDetails.js
@@ -19,17 +19,17 @@ class DataDetails extends Component {
     }
 
     componentWillReceiveProps(newProps) {
-    // console.log(this.props.match.params.personId);
-    // console.log(newProps.match.params.personId);
+        // console.log(this.props.match.params.personId);
+        // console.log(newProps.match.params.personId);
         if (this.props.match.params.personId !== newProps.match.params.personId) {
             this.setState({
                 loading: true
             });
             this.getTheInfo(newProps.match.params.personId);
-        }   
+        }
     }
 
-    getTheInfo(personId) {
+    getTheInfo = (personId) => {
         axios.get(swApiUrl + personId)
             .then(response => {
                 console.log(response.data);
@@ -44,39 +44,39 @@ class DataDetails extends Component {
                     loading: false
                 })
             })
-        }
-
-render() {
-    const { person, errMsg, loading } = this.state;
-
-    const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, url } = this.state.person;
-
-    if (loading) {
-        return <p>Loading...</p>
-    } else if (errMsg) {
-        return <p>Data details no workie workie</p>
-    } else {
-        return (
-            <div className="nerdinfo">
-                <h1>{this.state.person.name}</h1>
-                <p>Height</p>
-                <p>{height}</p>
-                <p>Mass</p>
-                <p>{mass}</p>
-                <p>Hair Color</p>
-                <p>{hair_color}</p>
-                <p>Skin Color</p>
-                <p>{skin_color}</p>
-                <p>Eye Color</p>
-                <p>{eye_color}</p>
-                <p>Birth Year</p>
-                <p>{birth_year}</p>
-                <p>Gender</p>
-                <p>{gender}</p>
-           </div>
-        )
     }
-}
+
+    render() {
+        const { person, errMsg, loading } = this.state;
+
+        const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, url } = this.state.person;
+
+        if (loading) {
+            return <p>Loading...</p>
+        } else if (errMsg) {
+            return <p>Data details no workie workie</p>
+        } else {
+            return (
+                <div className="nerdinfo">
+                    <h1>{this.state.person.name}</h1>
+                    <p>Height</p>
+                    <p>{height}</p>
+                    <p>Mass</p>
+                    <p>{mass}</p>
+                    <p>Hair Color</p>
+                    <p>{hair_color}</p>
+                    <p>Skin Color</p>
+                    <p>{skin_color}</p>
+                    <p>Eye Color</p>
+                    <p>{eye_color}</p>
+                    <p>Birth Year</p>
+                    <p>{birth_year}</p>
+                    <p>Gender</p>
+                    <p>{gender}</p>
+                </div>
+            )
+        }
+    }
 }
 
 export default DataDetails;
