185c895fcaa2d20bc65cbc82be9cc46db2b6b8d0
JE <28377077+vnsn@users.noreply.github.com>
2018-04-08 03:38:42 -0600

    worked on redux timer

--- exercises/d28-02-redux-timer/src/index.js
+++ exercises/d28-02-redux-timer/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
+import { Provider } from "react-redux";
 import store from "./redux";
+
 import App from "./App";
 import "./style.css";
 
